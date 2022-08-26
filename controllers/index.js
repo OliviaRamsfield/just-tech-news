43c8cffbee1121ab9dd8f72af9da98cb777d37a1
Olivia <ramsfol@gmail.com>
2022-08-25 19:50:20 -0500

    included homepage HTML template

--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 //imports
 const router = require('express').Router()
 const apiRoutes = require('./api')
+const homeRoutes = require('./home-routes.js')
 
 //prefix end point with /api
-router.use('/api', apiRoutes)
+router.use('/api', apiRoutes);
+router.use('/', homeRoutes);
 
 //make a request that doesn't exist and get a 404 error message
 router.use((req, res) => {
